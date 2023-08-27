c03ffa63230a73a01065a8ecb79404aef10dbde9
pranav-chandrode <pchandrode2002@gmail.com>
2023-08-27 16:00:09 +0530

    decoder.py

--- decoder.py
+++ decoder.py
@@ -1,5 +1,5 @@
 from utils import TextProcess
-from pyctcdecode  import build_ctcdecoder
+import ctcdecode
 
 textprocess = TextProcess()
 
@@ -39,13 +39,19 @@ class CTCBeamDecoder:
 
     def __init__(self, beam_size=100, blank_id=labels.index('_'), kenlm_path=None):
         print("loading beam search with Language model...")
-        self.decoder = build_ctcdecoder(labels=labels,kenlm_model_path=kenlm_path)
+        self.decoder = ctcdecode.CTCBeamDecoder(labels=labels,model_path=kenlm_path,
+                                                alpha=0.522729216841, beta=0.96506699808,
+                                                beam_width=beam_size, blank_id=labels.index('_'))
      
         print("finished loading beam search")
 
-    # def __call__(self, output):
-    def deconding(self,output):
-        # output.squeeze(0)
-        # output = output.transpose(0,1)
-        text = self.decoder.decode(output)
-        return text
+    def __call__(self, output):
+        beam_results, beam_scores, timesteps, out_seq_len = self.decoder.decode(output)
+        return self.convert_to_string(beam_results[0][0],labels, out_seq_len[0][0])
+        # # output.squeeze(0)
+        # # output = output.transpose(0,1)
+        # text = self.decoder.decode(output)
+        # return text
+
+    def convert_to_string(self,tokens, vocab, seq_len):
+        return "".join([vocab[x] for  x in tokens[0:seq_len]])
